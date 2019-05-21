3279fc856cf76ad50dca3d0c9af43fb4ee7a3a15
静水峰 <703786074@qq.com>
2019-05-21 23:43:45 +0800

    封装自定义组件

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Sjld from './components/xin.vue'
+const sjld={
+    install:function (Vue){
+        Vue.component('sjld',Sjld)
+    }
+}
+export default sjld
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import sjld from './index'
+Vue.use(sjld);
 
 Vue.config.productionTip = false
 
